4d59fdda4e61fcc3b65d991b762376c546fc5bd4
Mindy Gottlieb <mindy@mindys-mbp.myfiosgateway.com>
2020-05-10 22:58:16 -0400

    finished CovidTracker application

--- src/main/java/gottlieb/covidTracker/CovidTrackerController.java
+++ src/main/java/gottlieb/covidTracker/CovidTrackerController.java
@@ -4,15 +4,14 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import javax.swing.*;
 import java.util.List;
 
 public class CovidTrackerController {
     private CovidTrackerService service;
-    private CovidTrackerView view;
 
-    public CovidTrackerController(CovidTrackerService service, CovidTrackerView view) {
+    public CovidTrackerController(CovidTrackerService service) {
         this.service = service;
-        this.view = view;
     }
 
     public void requestGlobalData() {
@@ -21,7 +20,14 @@ public class CovidTrackerController {
             @Override
             public void onResponse(Call<CovidTracker> call, Response<CovidTracker> response) {
                 CovidTracker.Global global = response.body().global;
-                view.setGlobal(global);
+                CovidTrackerFrame.title.setText("World");
+                CovidTrackerFrame.newConfirmed.setText("New confirmed: " + String.valueOf(global.newConfirmed));
+                CovidTrackerFrame.totalConfirmed.setText("Total confirmed: " + String.valueOf(global.totalConfirmed));
+                CovidTrackerFrame.newDeaths.setText("New deaths: " + String.valueOf(global.newDeaths));
+                CovidTrackerFrame.totalDeaths.setText("Total deaths: " + String.valueOf(global.totalDeaths));
+                CovidTrackerFrame.newRecovered.setText("New recovered: " + String.valueOf(global.newRecovered));
+                CovidTrackerFrame.totalRecovered.setText("Total recovered: " + String.valueOf(global.totalRecovered));
+                CovidTrackerFrame.errorMessage.setText("");
             }
 
             @Override
@@ -37,8 +43,28 @@ public class CovidTrackerController {
             @Override
             public void onResponse(Call<CovidTracker> call, Response<CovidTracker> response) {
                 List<CovidTracker.Countries> countries = response.body().countries;
-                CovidTracker.Countries country = countries.get(findCountry(countries, 0, countries.size(), userCountry));
-                view.setCountry(country);
+                try {
+                    CovidTracker.Countries country = countries.get(findCountry(countries, 0, countries.size(), userCountry));
+                    CovidTrackerFrame.title.setText(country.country);
+                    CovidTrackerFrame.newConfirmed.setText("New confirmed: " + String.valueOf(country.newConfirmed));
+                    CovidTrackerFrame.totalConfirmed.setText("Total confirmed: " + String.valueOf(country.totalConfirmed));
+                    CovidTrackerFrame.newDeaths.setText("New deaths: " + String.valueOf(country.newDeaths));
+                    CovidTrackerFrame.totalDeaths.setText("Total deaths: " + String.valueOf(country.totalDeaths));
+                    CovidTrackerFrame.newRecovered.setText("New recovered: " + String.valueOf(country.newRecovered));
+                    CovidTrackerFrame.totalRecovered.setText("Total recovered: " + String.valueOf(country.totalRecovered));
+                    CovidTrackerFrame.errorMessage.setText("");
+                }
+                catch (StackOverflowError e)
+                {
+                    CovidTrackerFrame.title.setText("");
+                    CovidTrackerFrame.newConfirmed.setText("");
+                    CovidTrackerFrame.totalConfirmed.setText("");
+                    CovidTrackerFrame.newDeaths.setText("");
+                    CovidTrackerFrame.totalDeaths.setText("");
+                    CovidTrackerFrame.newRecovered.setText("");
+                    CovidTrackerFrame.totalRecovered.setText("");
+                    CovidTrackerFrame.errorMessage.setText("Invalid country. Please try again.");
+                }
             }
 
             @Override
@@ -50,20 +76,15 @@ public class CovidTrackerController {
 
 
     private int findCountry(List<CovidTracker.Countries> countries, int start, int size, String country) {
-        int mid = (start + size) / 2;
+            int mid = (start + size) / 2;
 
-        if (country.equalsIgnoreCase(countries.get(mid).country))
-        {
-            return mid;
-        }
-        else if ((country.compareToIgnoreCase(countries.get(mid).country)) <= -1)
-        {
-            return findCountry(countries, 0, mid, country);
-        }
-        else if ((country.compareToIgnoreCase(countries.get(mid).country)) >= 1)
-        {
-            return findCountry(countries, mid, size, country);
-        }
+            if (country.equalsIgnoreCase(countries.get(mid).country)) {
+                return mid;
+            } else if ((country.compareToIgnoreCase(countries.get(mid).country)) <= -1) {
+                return findCountry(countries, 0, mid, country);
+            } else if ((country.compareToIgnoreCase(countries.get(mid).country)) >= 1) {
+                return findCountry(countries, mid, size, country);
+            }
         return -1;
     }
 }
--- src/main/java/gottlieb/covidTracker/CovidTrackerFrame.java
+++ src/main/java/gottlieb/covidTracker/CovidTrackerFrame.java
@@ -8,19 +8,36 @@ import java.awt.*;
 
 public class CovidTrackerFrame extends JFrame {
 
-
     JTextField country;
     JButton getCountry;
     JButton getGlobal;
 
+    public static JLabel title;
+    public static JLabel newConfirmed;
+    public static JLabel totalConfirmed;
+    public static JLabel newDeaths;
+    public static JLabel totalDeaths;
+    public static JLabel newRecovered;
+    public static JLabel totalRecovered;
+    public static JLabel errorMessage;
+
     public CovidTrackerFrame() {
-        setSize(600, 800);
+        setSize(600, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Covid-19 Stats");
         setLayout(new BorderLayout());
 
-        CovidTrackerView view = new CovidTrackerView();
-        add(view.panel, BorderLayout.CENTER);
+        JPanel info = new JPanel();
+        add(info, BorderLayout.CENTER);
+        info.setLayout(new BoxLayout(info, BoxLayout.PAGE_AXIS));
+        info.add(title = new JLabel(""));
+        info.add(newConfirmed = new JLabel(""));
+        info.add(totalConfirmed = new JLabel(""));
+        info.add(newDeaths = new JLabel(""));
+        info.add(totalDeaths = new JLabel(""));
+        info.add(newRecovered = new JLabel(""));
+        info.add(totalRecovered = new JLabel(""));
+        info.add(errorMessage = new JLabel(""));
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.covid19api.com//")
@@ -28,7 +45,7 @@ public class CovidTrackerFrame extends JFrame {
                 .build();
         CovidTrackerService service = retrofit.create((CovidTrackerService.class));
 
-        CovidTrackerController covidTrackerController = new CovidTrackerController(service, view);
+        CovidTrackerController covidTrackerController = new CovidTrackerController(service);
 
 
         JPanel chooseOutput = new JPanel();
@@ -41,12 +58,14 @@ public class CovidTrackerFrame extends JFrame {
         chooseOutput.add(getCountry);
         getCountry.addActionListener(actionEvent -> {
             covidTrackerController.requestCountryData(country.getText());
+            country.setText("");
         });
 
         getGlobal = new JButton("Global Data");
         chooseOutput.add(getGlobal);
         getGlobal.addActionListener(actionEvent -> {
             covidTrackerController.requestGlobalData();
+            country.setText("");
         });
     }
 
